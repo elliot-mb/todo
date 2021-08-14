2528f240f48048313ba197aef00fe527ea0688f9
unknown <elliotbuckingham14@gmail.com>
2021-08-14 23:30:41 +0100

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,73 @@
+<!-- run this app locally https://youtu.be/WmrawkHYMTg?t=121 -->
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+
+    <strong>Things to do</strong>
+
+    <form v-on:submit.prevent="doneTodo">
+      <ul>
+        <li v-for="(todo, index) in todos" v-bind:key="todo">
+          <button id="x-button" type="submit" v-on:click="doneIndex=index">X</button>
+          {{ todo }}
+        </li>
+      </ul>
+    </form>
+
+    <form v-on:submit.prevent="addTodo">
+      <input v-model="todoText" placeholder="What needs to be done?"> <!-- vue-model directive binds input todoText to form input value -->
+      <button type="submit" v-if="todoText != ''">Add Todo</button>
+    </form>
+
+  <button v-on:click.prevent="clearTodo">Clear all Todos</button>
+
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script> //component represented as a plain javascript object (POJO) like a class but plain and boring
+export default{
+  name: "HelloWorld", //reactive data or state
+  data: function(){
+    return{
+      todos:[],
+      todoText:'',
+      doneIndex:0,
+    };
+  },
+  methods: { //to change the state of this object
+    addTodo: function(){
+      this.todos = [...this.todos, this.todoText]; //appends todoText of element to array
+      this.todoText = '';
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    doneTodo: function(){
+      this.todos.splice(this.doneIndex, 1);
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    clearTodo: function(){
+      this.todos = [];
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+  },
+  mounted: function(){
+    const existingTodos = localStorage.getItem('todos');
+    this.todos = JSON.parse(existingTodos) || [];
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+
+#x-button{
   padding: 0;
+  border: none;
+  background: none;
+  color: purple;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+li{
+  list-style-type: none;
+  padding-right: 35px;
 }
-a {
-  color: #42b983;
-}
+
 </style>
